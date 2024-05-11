673fdabf00abd48baa448bb3601eeb1d06b749b6
NiX <neplaz.dimitri@gmail.com>
2024-05-11 16:16:52 +0200

    clean code

---
+++ apps/front/src/app/post/page.tsx
@@ -0,0 +1,9 @@
+export default async function Post() {
+
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <h1>Post API exemple</h1>
+      </main>
+    );
+  }
+  
