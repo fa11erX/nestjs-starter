0c0099efcd43ec36ce0614809c3aaf9121b571df
NiX <neplaz.dimitri@gmail.com>
2024-05-11 13:31:09 +0200

    init prisma client and migration

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UserService } from './user.service';
+import { User as UserModel } from '@prisma/client';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly userService: UserService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('user')
+  async signupUser(
+    @Body() userData: { name?: string; email: string },
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
   }
 }
